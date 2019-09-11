6a7ea6db280ac26d89b62e74c1c4d89937161113
Adam Dudczak <adam.dudczak@allegro.pl>
2019-09-11 21:36:35 +0200

    meta evolve tested

--- 03_evolution/meta_evolve.sh
+++ 03_evolution/meta_evolve.sh
@@ -5,9 +5,10 @@ population=1
 base_dir_name="./evolution"
 logs_dir="${base_dir_name}/logs"
 
-while [[ ${population} -le 10 ]]
+while [[ ${population} -le 2 ]]
 do
-	cp -R "${base_dir_name}/template" "${base_dir_name}/${population}_experiment"
+	cp -R "${base_dir_name}/template_fixed_game" "${base_dir_name}/${population}_experiment"
+#	cp -R "${base_dir_name}/template" "${base_dir_name}/${population}_experiment"
 	date
 	bash evolve.sh "${population}_experiment" > "${base_dir_name}/${population}_experiment/experiment.log"
 	date
