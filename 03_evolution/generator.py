55d8f11b58ab4492d8ef44622718f5b0c1ca9c6d
Adam Dudczak <adam.dudczak@allegro.pl>
2019-09-11 08:12:00 +0200

    fixed length game evolution

--- 03_evolution/generator.py
+++ 03_evolution/generator.py
@@ -49,6 +49,30 @@ def new_moves_generator_smok(moves_to_extend, variant):
     return moves_to_extend
 
 
+def crossover(parent1_moves, parent2_moves):
+    offspring_moves = parent1_moves.copy()
+    noise = random.randint(0, 1)
+
+    for i in range(0, len(parent2_moves)):
+        if noise == 1 and i % 2 == 0:
+            offspring_moves[i] = parent2_moves[i]
+
+        if noise == 0 and i % 2 == 1:
+            offspring_moves[i] = parent2_moves[i]
+
+    return offspring_moves
+
+
+def mutate(offspring_moves, how_many_mutations):
+    mutant_moves = offspring_moves.copy()
+
+    for i in range(0, how_many_mutations):
+        mutation_idx = random.randint(0, len(offspring_moves))
+        mutant_moves[mutation_idx] = "\n" + str(random.randint(0, 5))
+
+    return mutant_moves
+
+
 def random_moves(how_many):
     sequence = ""
     for i in range(0, how_many):
@@ -61,7 +85,8 @@ def random_moves(how_many):
 # possible moves
 # 0 - left, 1 - right, 2 - shoot, 3 - nothing,
 # 4 - left + shoot, 5 - right + shoot
-def evolve(candidate, generation):
+def evolve(candidates, generation):
+    candidate = candidates[0]
     moves = load_moves(candidate["seq_file"])
     next_generation = list()
     variants_and_moves = {"1": 0, "2": 1, "3": 2, "4": 4, "5": 5}
@@ -76,6 +101,42 @@ def evolve(candidate, generation):
         new_candidate["moves"] = new_moves_generator_smok(moves.copy(), v)
         next_generation.append(new_candidate)
     return next_generation
+
+
+def evolve_fixed_length(candidates, generation):
+    best_parent1 = candidates[0]
+    best_parent1_moves = load_moves(best_parent1["seq_file"])
+
+    best_parent2 = candidates[1]
+    best_parent2_moves = load_moves(best_parent2["seq_file"])
+
+    best_parent3 = candidates[2]
+    best_parent3_moves = load_moves(best_parent3["seq_file"])
+
+    offspring_1_2_moves = crossover(best_parent1_moves, best_parent2_moves)
+    offspring_3_1_moves = crossover(best_parent3_moves, best_parent1_moves)
+    offspring_2_3_moves = crossover(best_parent2_moves, best_parent3_moves)
+    mutant_1_2_moves = mutate(offspring_1_2_moves, random.randint(100, 200))
+    mutant_3_1_moves = mutate(offspring_3_1_moves, random.randint(100, 200))
+
+    new_generation = int(generation) + 1
+    new_candidates = list()
+
+    new_candidates.append(get_offspring(offspring_1_2_moves, 1, new_generation))
+    new_candidates.append(get_offspring(offspring_3_1_moves, 2, new_generation))
+    new_candidates.append(get_offspring(offspring_2_3_moves, 3, new_generation))
+    new_candidates.append(get_offspring(mutant_1_2_moves, 4, new_generation))
+    new_candidates.append(get_offspring(mutant_3_1_moves, 5, new_generation))
+
+    return new_candidates
+
+
+def get_offspring(moves, variant, generation):
+    new_candidate = dict()
+    new_candidate["generation"] = generation
+    new_candidate["variant"] = variant
+    new_candidate["moves"] = moves
+    return new_candidate
 
 
 # new_moves = [ { "generation", "variant", "moves" }, .. ]
@@ -96,6 +157,9 @@ if generation > 1:
 
 generations_metada.update(sc.parse_generation_metadata(generation, path_to_evolution))
 
-candidate = sc.get_best_seqs(generations_metada, "score")
-new_moves = evolve(candidate, generation)
+candidates = sc.get_best_candidates(generations_metada, "score")
+
+# todo test both games scenario
+# new_moves = evolve(candidates, generation)
+new_moves = evolve_fixed_length(candidates, generation)
 save_new_moves(new_moves, path_to_evolution)
